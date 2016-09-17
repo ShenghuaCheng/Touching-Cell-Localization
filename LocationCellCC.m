c652c38defb5934bab7fbf24865fb8bdc2418f6e
ShenghuaCheng <905806158@qq.com>
2016-09-17 19:52:26 +0800

    Update LocationCellCC.m

--- LocationCellCC.m
+++ LocationCellCC.m
@@ -1,4 +1,4 @@
-function cellStruct = LocationCellCC(imgOrig, tempPixelList, thetaGauss, volumeThre, minR, selectThre, localInd1, localInd2, localSize, localNum)
+function cellStruct = LocationCellCC(initialInd, connectedCImg, tempPixelList, thetaGauss, volumeThre, minR, selectThre, localInd1, localInd2, localSize, localNum)
 cellStruct.center = [0 0 0];
 cellStruct.centerRe = [0 0 0];
 cellStruct.volume = 0;
@@ -6,7 +6,7 @@ cellStruct.element{1, 1} = zeros(3, 3);
 cellStruct.radius = 0;
 cellStruct.label = 0;
 if size(tempPixelList, 1) >= volumeThre && IsCoplanar(tempPixelList) == 0
-    [initialInd, connectedCImg] = ExtractMinBox(tempPixelList, imgOrig);
+    %[initialInd, connectedCImg] = ExtractMinBox(tempPixelList, imgOrig);
     cellStruct = LocationCell(connectedCImg, thetaGauss, volumeThre, cellStruct, initialInd, minR, selectThre, localInd1, localInd2, localSize, localNum);
 else
     numCell = size(cellStruct.center, 1);
